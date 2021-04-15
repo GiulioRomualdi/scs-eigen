4722df58612f44ef864d51704c89e8d346e9c3d7
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-04-16 00:42:13 +0200

    Implement the CMake skeleton

---
+++ cmake/ScsEigenDependencies.cmake
@@ -0,0 +1,8 @@
+
+#---------------------------------------------
+## Required Dependencies
+find_package(Eigen3 3.2.92 REQUIRED)
+find_package(scs REQUIRED)
+
+#---------------------------------------------
+## Optional Dependencies
